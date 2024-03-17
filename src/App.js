1e5412ba6740510cd466800416d5dc21791a5460
John Carl Balatan <145192890+Jeyseaa@users.noreply.github.com>
2024-03-17 08:31:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AuthForm from './AuthForm';
 import Dashboard from './Dashboard';
+import AboutUs from './AboutUs'; 
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AuthForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about-us" element={<AboutUs />} /> 
       </Routes>
     </Router>
   );
